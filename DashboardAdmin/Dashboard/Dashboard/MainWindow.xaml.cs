7b79caafa1e7a0572586606041b0f1448dca4f11
Hossynkoala <Hossynassghary@gmail.com>
2020-10-06 01:00:43 +0330

    Cheackpoint

--- DashboardAdmin/Dashboard/Dashboard/MainWindow.xaml.cs
+++ DashboardAdmin/Dashboard/Dashboard/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Windows;
+﻿using DashboardAdmin.Dashboard.Dashboard.SubpageStatices;
+using DashboardAdmin.Dashboard.PageAut;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 
 namespace DashboardAdmin
 {
@@ -7,11 +16,133 @@ namespace DashboardAdmin
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow Dashboard;
+
+        UserControl CurentPage;
+        TextBlock CurentTab;
+        BlurEffect Blur=new BlurEffect();
+
         public MainWindow()
         {
             InitializeComponent();
+            Dashboard = this;
+            CurentPage = new SubpageStatices();
+            CurentTab = BTNStatices;
+            Root.Children.Add(new Login());
+
+
         }
 
+        internal async void Blure(bool OnOff)
+        {
+            if (OnOff)
+            {
+                PageDashboard.Effect = Blur;
+                while (Blur.Radius < 10)
+                {
+                    Blur.Radius += 0.5d;
+                    await Task.Delay(4);
+
+                    if (Blur.Radius >= 10)
+                        break;
+                }
+            }
+            else
+            {
+                while (Blur.Radius > 0)
+                {
+                    Blur.Radius -= 0.5d;
+                    await Task.Delay(4);
+
+                    if (Blur.Radius <= 0)
+                    {
+                        PageDashboard.Effect = null;
+                        break;
+
+                    }
+                }
+
+            }
+        }
+
+
+        public void ChangeColor_Active(object sender, MouseEventArgs e)
+        {
+            var TextBlock = sender as TextBlock;
+            ColorAnimation Anim = new ColorAnimation(fromValue: Colors.Gray, toValue: Colors.Orange, TimeSpan.FromSeconds(0.3));
+            Storyboard.SetTargetName(Anim, TextBlock.Name);
+            Storyboard.SetTargetProperty(Anim, new PropertyPath("(TextBlock.Foreground).(SolidColorBrush.Color)"));
+
+            DoubleAnimation Anim1 = new DoubleAnimation(18, 20, TimeSpan.FromSeconds(0.1));
+            Storyboard.SetTargetName(Anim1, TextBlock.Name);
+            Storyboard.SetTargetProperty(Anim1, new PropertyPath("FontSize"));
+
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(Anim);
+            storyboard.Children.Add(Anim1);
+
+            storyboard.Begin(this);
+
+        }
+
+        public void ChangeColor_DeActive(object sender, MouseEventArgs e)
+        {
+            var TextBlock = sender as TextBlock;
+
+            ColorAnimation Anim = new ColorAnimation(fromValue: Colors.Orange, toValue: Colors.Gray, TimeSpan.FromSeconds(0.3));
+            Storyboard.SetTargetName(Anim, TextBlock.Name);
+            Storyboard.SetTargetProperty(Anim, new PropertyPath("(TextBlock.Foreground).(SolidColorBrush.Color)"));
+
+            DoubleAnimation Anim1 = new DoubleAnimation(20, 18, TimeSpan.FromSeconds(0.1));
+            Storyboard.SetTargetName(Anim1, TextBlock.Name);
+            Storyboard.SetTargetProperty(Anim1, new PropertyPath("FontSize"));
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(Anim);
+            storyboard.Children.Add(Anim1);
+            storyboard.Begin(this);
+
+        }
+
+        public void ControlPane(object sender, MouseButtonEventArgs e)
+        {
+            Storyboard storyboard = new Storyboard();
+
+            if (NameList.Width >= 100)
+            {
+                DoubleAnimation Anim = new DoubleAnimation(100, 0, TimeSpan.FromSeconds(0.3));
+                Storyboard.SetTargetName(Anim, NameList.Name);
+                Storyboard.SetTargetProperty(Anim, new PropertyPath("Width"));
+                storyboard.Children.Add(Anim);
+            }
+            else
+            {
+                DoubleAnimation Anim = new DoubleAnimation(0, 100, TimeSpan.FromSeconds(0.3));
+                Storyboard.SetTargetName(Anim, NameList.Name);
+                Storyboard.SetTargetProperty(Anim, new PropertyPath("Width"));
+                storyboard.Children.Add(Anim);
+            }
+
+            storyboard.Begin(this);
+        }
+
+
+        private void Window_LayoutUpdated(object sender, EventArgs e)
+        {
+            if (NameList.Width >= 100)
+            {
+                BTNOpenPane.Foreground = new SolidColorBrush(Colors.Orange);
+                BTNOpenPane.Text = "\xEA49";
+            }
+            else
+            {
+                BTNOpenPane.Text = "\xEA5B";
+            }
+
+            CurentTab.Foreground = new SolidColorBrush(Colors.Orange);
+
+        }
 
     }
 }
---
+++ DashboardAdmin/Dashboard/PageAut/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DashboardAdmin.Dashboard.PageAut
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+            MainWindow.Dashboard.Blure(true);
+        }
+
+        public void LoginAction(object sender,MouseButtonEventArgs e)
+        {
+
+        }
+
+    }
+}
